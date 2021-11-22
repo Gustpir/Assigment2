de16f77604327adafe76e64bc2f7a71f5ce99234
yos prabowo <yepprabowo@gmail.com>
2021-11-23 00:09:31 +0700

    assignment 2

---
+++ const.js
@@ -0,0 +1,25 @@
+(function(global) {
+    "use strict;"
+
+    // Class ------------------------------------------------
+    var Const = {};
+
+    // resource for building market
+    Const.goldMarket = 30;
+    Const.foodMarket = 10;
+
+
+    // resource for building farm
+    Const.goldFarm = 10;
+    Const.foodFarm = 30;
+
+
+    // resource for building barrack
+    Const.goldBarrack = 30;
+    Const.foodBarrack = 30;
+
+
+    // Exports ----------------------------------------------
+    module["exports"] = Const;
+
+})((this || 0).self || global);
